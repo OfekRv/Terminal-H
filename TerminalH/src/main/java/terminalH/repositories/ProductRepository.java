861f8b7a306e9196d4c423a18cd9af9bf6f969d7
ofekr <ofekrv@gmail.com>
2021-03-31 12:02:42 +0300

    sort on search

--- TerminalH/src/main/java/terminalH/repositories/ProductRepository.java
+++ TerminalH/src/main/java/terminalH/repositories/ProductRepository.java
@@ -1,19 +1,19 @@
 package terminalH.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 import terminalH.entities.Product;
 
-import java.util.Collection;
-
 @Repository
 @RepositoryRestResource(collectionResourceRel = "products", path = "products")
 public interface ProductRepository extends JpaRepository<Product, Long> {
     boolean existsByUrl(String url);
 
-    Collection<Product> findByNameContainingIgnoreCase(@Param("name") String name);
+    Page<Product> findByNameContainingIgnoreCase(@Param("name") String name, Pageable page);
 
-    Collection<Product> findByDescriptionContainingIgnoreCase(@Param("desc") String desc);
+    Page<Product> findByDescriptionContainingIgnoreCase(@Param("desc") String desc, Pageable page);
 }
