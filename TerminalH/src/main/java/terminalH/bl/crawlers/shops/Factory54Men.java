45c1000db8a5f9f9b16955fe134265463ad81e1c
ofekr <ofekrv@gmail.com>
2021-04-03 13:50:15 +0300

    duplicate factory bugfix

--- TerminalH/src/main/java/terminalH/bl/crawlers/shops/Factory54Men.java
+++ TerminalH/src/main/java/terminalH/bl/crawlers/shops/Factory54Men.java
@@ -2,6 +2,7 @@ package terminalH.bl.crawlers.shops;
 
 import org.jsoup.nodes.Element;
 import org.springframework.beans.factory.annotation.Value;
+import terminalH.entities.Shop;
 import terminalH.entities.enums.Gender;
 
 import javax.inject.Named;
@@ -10,6 +11,12 @@ import javax.inject.Named;
 public class Factory54Men extends Factory54Crawler {
     @Value("${FACTORY54_MEN_CATEGORY_PATH}")
     private String menCategoryPath;
+
+    @Override
+    public Shop getShopToCrawl() {
+        return getShopRepository().findByUrl(super.getShopUrl()).
+                orElseGet(() -> getShopRepository().save(new Shop(getShopUrl(), getShopName())));
+    }
 
     @Override
     public Gender extractGender(Element product) {
