ef05bddf974a6e9081e73756bc635d3f91f68cb3
ofekr <ofekrv@gmail.com>
2021-04-09 00:01:18 +0300

    new /products search api

--- TerminalH/src/main/java/terminalH/repositories/ProductRepository.java
+++ TerminalH/src/main/java/terminalH/repositories/ProductRepository.java
@@ -1,17 +1,23 @@
 package terminalH.repositories;
 
+import com.querydsl.core.types.dsl.StringPath;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.stereotype.Repository;
 import terminalH.entities.Product;
+import terminalH.entities.QProduct;
 
 @Repository
 @RepositoryRestResource(collectionResourceRel = "products", path = "products")
-public interface ProductRepository extends JpaRepository<Product, Long> {
+public interface ProductRepository extends JpaRepository<Product, Long>, QuerydslPredicateExecutor<Product>,
+        QuerydslBinderCustomizer<QProduct> {
     @RestResource(exported = false)
     boolean existsByUrl(String url);
 
@@ -22,5 +28,8 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
 
     Page<Product> findByNameContainingIgnoreCase(@Param("name") String name, Pageable page);
 
-    Page<Product> findByDescriptionContainingIgnoreCase(@Param("desc") String desc, Pageable page);
+    @Override
+    default void customize(QuerydslBindings bindings, QProduct product) {
+        bindings.bind(product.name).first((name, value) -> name.containsIgnoreCase(value));
+    }
 }
---
+++ TerminalH/src/main/java/terminalH/repositories/specifications/ProductSpecification.java
@@ -0,0 +1,4 @@
+package terminalH.repositories.specifications;
+
+public class ProductSpecification {
+}
